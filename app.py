9657dc02f413188686bb86951c2a9694339a0832
Amit Kumar Gupta <hriks@users.noreply.github.com>
2017-08-21 00:00:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
 app.config.from_object('config')
 
 
-count_dict = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0,16:0,17:0,18:0,19:0,20:0} # noqa
+count_dict = {
+    1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,
+    11:0,12:0,13:0,14:0,15:0,16:0,17:0,18:0,19:0,20:0
+} # noqa
 
 # Shutdown Server
 def shutdown_server():
